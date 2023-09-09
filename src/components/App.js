13ff1dd631effdbb22d3af96443a23389e9d4ba7
ptrodion <rdnpetrenko@gmail.com>
2023-09-09 16:13:23 +0100

    add finally version

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 
 import { GlobalStyle } from './GlobalStyle';
@@ -7,73 +7,59 @@ import { ContactForm } from './FormContacts/ContactsForm';
 import { Filter } from './Filter/Filter';
 import { ContactsList } from './ContactsList/ContactsList';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts') || [])
+  );
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
-    const savedOurContacts = localStorage.getItem('contacts');
-    if (savedOurContacts !== null) {
-      this.setState({
-        contacts: JSON.parse(savedOurContacts),
-      });
-    }
-  }
+  // useEffect(() => {
+  //   const savedOurContacts = localStorage.getItem('contacts');
+  //   console.log(savedOurContacts);
+  //   if (savedOurContacts !== null) {
+  //     setContacts(JSON.parse(savedOurContacts));
+  //   }
+  // }, []);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  addContact = newContact => {
-    const isThisNameNotUnique = this.state.contacts.some(
-      telName => telName.name.toUpperCase() === newContact.name.toUpperCase()
+  const addContact = newContact => {
+    const isNameNotUnique = contacts.some(
+      contact => contact.name.toUpperCase() === newContact.name.toUpperCase()
     );
 
-    if (!isThisNameNotUnique) {
-      this.setState(prevState => ({
-        contacts: [...prevState.contacts, { id: nanoid(5), ...newContact }],
-      }));
+    if (!isNameNotUnique) {
+      setContacts(prevContacts => [
+        ...prevContacts,
+        { id: nanoid(5), ...newContact },
+      ]);
     } else {
       alert(`${newContact.name} is already in contacts`);
     }
   };
 
-  changeContactsFilter = newFilter => {
-    this.setState({
-      filter: newFilter,
-    });
+  const changeContactsFilter = newFilter => {
+    setFilter(newFilter);
   };
 
-  deleteContact = contact => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(
-        contactId => contactId.id !== contact
-      ),
-    }));
+  const deleteContact = contact => {
+    setContacts(prevState =>
+      prevState.filter(contactId => contactId.id !== contact)
+    );
   };
 
-  render() {
-    const filteredContacts = this.state.contacts.filter(contact =>
-      contact.name.toUpperCase().includes(this.state.filter.toUpperCase())
-    );
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toUpperCase().includes(filter.toUpperCase())
+  );
 
-    return (
-      <Layout>
-        <ContactForm onAdd={this.addContact} />
-        <Filter
-          filter={this.state.filter}
-          onFilter={this.changeContactsFilter}
-        />
-        <ContactsList
-          allContacts={filteredContacts}
-          onDelete={this.deleteContact}
-        />
-        <GlobalStyle />
-      </Layout>
-    );
-  }
-}
+  return (
+    <Layout>
+      <ContactForm onAdd={addContact} />
+      <Filter filter={filter} onFilter={changeContactsFilter} />
+      <ContactsList allContacts={filteredContacts} onDelete={deleteContact} />
+      <GlobalStyle />
+    </Layout>
+  );
+};
